6e6f723b9da972a5e219dfff2322c58ec34513e7
Victor Santiago <victorsantiago@Victors-MacBook-Pro-2.local>
2025-05-30 16:43:07 -0600

    add s3 plugin

--- config/plugins.js
+++ config/plugins.js
@@ -6,10 +6,14 @@ module.exports = ({ env }) => ({
   },
   upload: {
     config: {
-      provider: "local",
-      actionOptions: {
-        upload: {},
-        delete: {},
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: env('AWS_REGION'),
+        params: {
+          Bucket: env('AWS_BUCKET'),
+        },
       },
     },
   },
