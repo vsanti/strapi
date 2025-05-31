3dc96929197aba083c2c7f046238a10cd86656dc
Victor Santiago <victorsantiago@Victors-MacBook-Pro-2.local>
2025-05-31 14:41:55 -0600

    fixed s3 permissions

--- config/plugins.js
+++ config/plugins.js
@@ -9,7 +9,7 @@ module.exports = ({ env }) => ({
       provider: 'aws-s3',
       providerOptions: {
         accessKeyId: env('AWS_ACCESS_KEY_ID'),
-        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        secretAccessKey: env('AWS_SECRET_ACCESS_KEY'), // <-- FIXED
         region: env('AWS_REGION'),
         params: {
           Bucket: env('AWS_BUCKET'),
